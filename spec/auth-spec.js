15ce4359961de41ba3c9280292d56f4848857301
Michael Jackson <mjijackson@gmail.com>
2013-03-06 10:08:56 -0800

    Add connect spec

--- spec/auth-spec.js
+++ spec/auth-spec.js
@@ -13,26 +13,26 @@ describe('when a server requires auth', function () {
   });
 
   describe('a new client with the correct password', function () {
-    var db2;
+    var client;
     beforeEach(function () {
-      db2 = redis.createClient({ host: db.host, port: db.port, password: password });
+      client = redis.createClient({ host: db.host, port: db.port, password: password });
     });
 
     it('is able to issue commands successfully', function () {
-      return db2.get('a-key').then(function (value) {
+      return client.get('a-key').then(function (value) {
         assert.strictEqual(value, null);
       });
     });
   });
 
   describe('a new client with the wrong password', function () {
-    var db2;
+    var client;
     beforeEach(function () {
-      db2 = redis.createClient({ host: db.host, port: db.port });
+      client = redis.createClient({ host: db.host, port: db.port });
     });
 
     it('is able to issue commands successfully', function () {
-      return db2.get('a-key').then(function (value) {
+      return client.get('a-key').then(function (value) {
         assert(false, 'client issued command without auth');
       }, function (error) {
         assert(error);
