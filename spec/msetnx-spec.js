25fbb85115282b9082ee0d155f51bfa5f0c91416
Michael Jackson <mjijackson@gmail.com>
2013-03-06 11:16:53 -0800

    MSETNX accepts a hash

---
+++ spec/msetnx-spec.js
@@ -0,0 +1,39 @@
+require('./helper');
+
+describe('msetnx', function () {
+  describe('when some of the keys already exist', function () {
+    beforeEach(function () {
+      return db.set('a', 'one');
+    });
+
+    it('returns 0', function () {
+      return db.msetnx('a', 'one', 'b', 'two', 'c', 'three').then(function (reply) {
+        assert.strictEqual(reply, 0);
+      });
+    });
+  });
+
+  describe('when none of the keys already exist', function () {
+
+    describe('when given a list of fields/values', function () {
+      it('sets all keys to the correct values', function () {
+        return db.msetnx('a', 'one', 'b', 'two', 'c', 'three').then(function () {
+          return db.mget('a', 'b', 'c').then(function (values) {
+            assert.deepEqual(values, [ 'one', 'two', 'three' ]);
+          });
+        });
+      });
+    });
+
+    describe('when given a hash', function () {
+      it('sets all keys to the correct values', function () {
+        return db.msetnx({ a: 'one', b: 'two', c: 'three' }).then(function () {
+          return db.mget('a', 'b', 'c').then(function (values) {
+            assert.deepEqual(values, [ 'one', 'two', 'three' ]);
+          });
+        });
+      });
+    });
+
+  });
+});
