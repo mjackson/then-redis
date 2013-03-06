192093476b8447d55e4bfdbed519b025f3ce621d
Michael Jackson <mjijackson@gmail.com>
2013-03-06 09:47:17 -0800

    Add transactions spec

---
+++ spec/transactions-spec.js
@@ -0,0 +1,37 @@
+require('./helper');
+
+describe('transactions', function () {
+  describe('when there is no error inside a transaction', function () {
+    it('executes the transaction successfully', function () {
+      db.multi();
+      db.incr('a');
+      db.incr('b');
+      return db.exec().then(function (reply) {
+        assert.deepEqual(reply, [ 1, 1 ]);
+      });
+    });
+  });
+
+  describe('when there is an error executing the transaction', function () {
+    it('returns the error', function () {
+      db.multi();
+      db.set('a', 'hello');
+      db.incr('a');
+      return db.exec().then(function (reply) {
+        assert.equal(reply.length, 2);
+        assert(reply[1] instanceof Error);
+      });
+    });
+  });
+
+  describe('when there is an error enqueueing the transaction', function () {
+    it('throws the error', function () {
+      db.multi();
+      return db.send('unknown-command').then(function () {
+        assert(false, 'successfully queued non-existent command');
+      }, function (error) {
+        assert(error);
+      });
+    });
+  });
+});
