e26a2f1dcb5fa6a05ef706b19f01e22843e22bc0
Michael Jackson <mjijackson@gmail.com>
2013-03-05 22:15:25 -0800

    HMSET optionally accepts a hash as its only argument

--- spec/hmget-spec.js
+++ spec/hmget-spec.js
@@ -1,11 +1,8 @@
 require('./helper');
 
 describe('hmget', function () {
-  beforeEach(function () {
-    return db.hmset('my-key', 'a', 'one', 'b', 'two', 'c', 'three');
-  });
-
   it('returns an array of values for the given keys', function () {
+    db.hmset('my-key', 'a', 'one', 'b', 'two', 'c', 'three');
     return db.hmget('my-key', 'a', 'b').then(function (array) {
       assert.deepEqual(array, [ 'one', 'two' ]);
     });
---
+++ spec/hmset-spec.js
@@ -0,0 +1,17 @@
+require('./helper');
+
+describe('hmset', function () {
+  it('accepts a list of fields/values as arguments', function () {
+    db.hmset('my-key', 'a', 'one', 'b', 'two');
+    return db.hgetall('my-key').then(function (hash) {
+      assert.deepEqual(hash, { a: 'one', b: 'two' });
+    });
+  });
+
+  it('accepts a hash as the only argument', function () {
+    db.hmset('my-key', { a: 'one', b: 'two' });
+    return db.hgetall('my-key').then(function (hash) {
+      assert.deepEqual(hash, { a: 'one', b: 'two' });
+    });
+  });
+});
