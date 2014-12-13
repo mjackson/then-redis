3859da08dbeb5af0ed712e7ada5cc0137fec0a19
Michael Jackson <mjijackson@gmail.com>
2014-12-12 21:59:42 -0800

    Better descriptions and argument names

--- modules/__tests__/lpush-test.js
+++ modules/__tests__/lpush-test.js
@@ -5,16 +5,16 @@ describe('lpush', function () {
   it('accepts a list of values as arguments', function () {
     db.lpush('my-key', 'c', 'b', 'a');
 
-    return db.lrange('my-key', 0, -1).then(function (hash) {
-      expect(hash).toEqual([ 'a', 'b', 'c' ]);
+    return db.lrange('my-key', 0, -1).then(function (list) {
+      expect(list).toEqual([ 'a', 'b', 'c' ]);
     });
   });
 
-  it('accepts a hash as the second argument', function () {
+  it('accepts an array as the second argument', function () {
     db.lpush('my-key', [ 'c', 'b', 'a' ]);
 
-    return db.lrange('my-key', 0, -1).then(function (hash) {
-      expect(hash).toEqual([ 'a', 'b', 'c' ]);
+    return db.lrange('my-key', 0, -1).then(function (list) {
+      expect(list).toEqual([ 'a', 'b', 'c' ]);
     });
   });
 });
--- modules/__tests__/rpush-test.js
+++ modules/__tests__/rpush-test.js
@@ -5,16 +5,16 @@ describe('rpush', function () {
   it('accepts a list of values as arguments', function () {
     db.rpush('my-key', 'a', 'b', 'c');
 
-    return db.lrange('my-key', 0, -1).then(function (hash) {
-      expect(hash).toEqual([ 'a', 'b', 'c' ]);
+    return db.lrange('my-key', 0, -1).then(function (list) {
+      expect(list).toEqual([ 'a', 'b', 'c' ]);
     });
   });
 
-  it('accepts a hash as the second argument', function () {
+  it('accepts an array as the second argument', function () {
     db.rpush('my-key', [ 'a', 'b', 'c' ]);
 
-    return db.lrange('my-key', 0, -1).then(function (hash) {
-      expect(hash).toEqual([ 'a', 'b', 'c' ]);
+    return db.lrange('my-key', 0, -1).then(function (list) {
+      expect(list).toEqual([ 'a', 'b', 'c' ]);
     });
   });
 });
